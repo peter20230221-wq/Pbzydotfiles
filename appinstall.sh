099c5089bbfd853b626d9c97271617d441910d4f
peter20230221-wq <liupeter20230221@gmail.com>
2026-01-10 00:18:11 +0900

    Add files via upload

---
+++ appinstall.sh
@@ -0,0 +1,44 @@
+#!/bin/bash
+yay -S \
+appimagelauncher \
+blueman \
+clash-verge-rev-bin \
+visual-studio-code-bin \
+copyq \
+dolphin \
+fastfetch \
+firefox \
+flatpak \
+foliate \
+foxitreader \
+freedownloadmanager \
+gnome-software \
+google-chrome-stable \
+hmcl \
+hyprland \
+kate \
+kd-bin \
+kitty \
+linuxqq \
+localsend \
+lutris \
+mako \
+missioncenter \
+mpv \
+neofetch \
+nm-applet \
+obsidian \
+qqmusic \
+spotify \
+steam \
+swww  \
+telegram-desktop \
+vim \
+waybar \
+waydroid \
+waypaper \
+wechat \
+wine \
+winetricks \
+wofi
+
